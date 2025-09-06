0d928da09f61f9a25574e3f01ae4acdef7c4e880
panda233133 <sk19722005@gmail.com>
2025-09-07 02:28:50 +0530

    more animations

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+function AnimationSetter(){
+    gsap.set("#nav a",{y:"-100%",opacity:0});
+    gsap.set("#home span .child",{y:"100%"})
+    gsap.set("#home .row img",{opacity:0})
+
+    gsap.set(".visual-text", {
+  opacity: 0,
+  strokeDasharray: 500,
+  strokeDashoffset: 500,
+  fill: "transparent",
+  strokeWidth: 2
+});
+    
+
+}
+
 const LoaderAnimation=()=>{
     const t2 = gsap.timeline(); 
 t2 
-.from(".child span", {     
+.from("#loader .child span", {     
     delay: 1,     
     stagger:.2,     
     duration: 1,     
     x: 100,     
     ease: "Power3.easeInOut" 
 }) 
-.to(".parent .child", {     
+.to("#loader .parent .child", {     
     duration: 1,     
     ease: "circ.inOut",     
     y: "-100%", 
@@ -29,7 +45,12 @@ t2
     duration: 1,     
     ease: "circ.inOut",     
     height:"0%", 
-    delay:-0.5
+    delay:-0.5,
+    onComplete(){
+        animateHomepage()
+    }
+
+  
 
 })
 }
@@ -46,7 +67,81 @@ function revealToSpan(){
         element.appendChild(parent);                      
     }); 
 }  
+// function animateSvg(){
+//     document.querySelectorAll("Visual>g").forEach(e=>{
+//         e.childNodes[1].childNodes[1].style.strokeDasharray=e.childNodes[1].childNodes[1].getTotalLength()+"px";
+//         e.childNodes[1].childNodes[1].style.strokeDashoffset=e.childNodes[1].childNodes[1].getTotalLength()+"px";
+//     })
 
-revealToSpan();  
+//     gsap.to("Visual>g>g>path",{
+//         strokeDashOffset:0,
+//         strokeDasharray:0,
+//         duration:2,
+//         ease:expo.easeInOut,
+//         delay:2
+//     })
+
+
+// }
+function animateSvg(){
+
+  gsap.timeline()
+    .to(".visual-text", {
+      opacity: 1,
+      visibility: "visible",
+      duration: "1"
+    })
+    .to(".visual-text", {
+      strokeDashoffset: 0,
+      duration: 3,
+      ease: "Power3.easeInOut"
+    })
+    .to(".visual-text", {
+      strokeWidth: 0,
+      fill: "#19c4ad",
+      duration: 0.4,
+      ease: Expo.easeInOut
+    });
+
+}
+function animateHomepage(){
+    
+
+    var t1=gsap.timeline()
+
+    t1.to("nav a ",{
+        y:0,
+        opacity:1,
+        stagger:.05,
+        ease:Expo.easeInOut,
+       
+    })
+    t1.to("#home .parent .child  ",{
+        y:0,
+        duration:2,
+        stagger:.01,
+        ease:Expo.easeInOut,
+        
+       
+    })
+     t1.to("#home img  ",{
+        opacity:1,
+        delay:-.5,
+        duration:"0.1",
+        ease:Expo.easeInOut,
+        OnComplete(){
+         animateSvg()
+        
+       
+    }
+        
+       
+    })
+}
+
+revealToSpan();
+AnimationSetter()  
 LoaderAnimation()
 
+
+// animateSvg()
